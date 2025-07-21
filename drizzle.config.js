a57efe658a435a294a557f673738826010a79768
m m <kmkl>
2025-07-21 11:16:54 -0400

    Your commit message

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+import { ENV } from "./src/config/env";
+
+export default {
+    schema: "./src/db/schema.js",
+    out: "./src/db/migrations",
+    dialect: "postgresql",
+    dbCredentials: {url: ENV.DATABASE_URL},
+
+}
